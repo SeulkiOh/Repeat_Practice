cc3951a29fd1ecda7ff00fdb0bb0e974fb8d68bd
SeulkiOh <ohseulki23@naver.com>
2019-01-26 11:47:28 +0900

    별찍기 기초 정답

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -67,6 +67,15 @@ public class MainDrive {
 //		*****
 //		*****
 //		*****
+		
+//		세로 줄 바꿈.
+		for (int i = 0; i < 5; i++) {
+//			가로 별 찍기.
+			for (int j = 0; j < 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	
 	
 	}
